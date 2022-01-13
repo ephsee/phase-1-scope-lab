323962341618872910e5fa0a121692f60d01a5f8
Francois Spies <fcspies@gmail.com>
2022-01-13 18:07:19 -0500

    commit tree 2257279984db3329732b30cc9a10e8b4244011b0

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Write your solution in this file!
+var customerName = 'bob'
+
+function upperCaseCustomerName() {
+    return customerName.toUpperCase()
+}
+upperCaseCustomerName()
+
+function setBestCustomer() {
+    var bestCustomer = 'not bob'
+    return bestCustomer
+}
+setBestCustomer()
+
+function overwriteBestCustomer() {
+    const bestCustomer = 'maybe bob'
+    return bestCustomer
+}
+overwriteBestCustomer()
+
+const leastFavoriteCustomer = 'patty o furniture'
+
+function changeLeastFavoriteCustomer() {
+    let leastFavoriteCustomer = 'bobby'
+    return leastFavoriteCustomer
+}
+changeLeastFavoriteCustomer()
